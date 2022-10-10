a9e4f34c46b0cd5997e7d27e6302e7c844f38bd8
Ekariorama Magaleta <bsc-com-07-19@unima.ac.mw>
2022-10-10 20:02:11 +0200

    For question 2

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -44,6 +44,7 @@ public class UserController {
         return userService.getUser(authentication);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_EXECUTIVE')")
     @PutMapping("update-role/{userName}/{role}")
     public ResponseEntity<ApiResponse> updateUserRole(@PathVariable("userName") String userName,
                                                       @PathVariable("role") String role,
